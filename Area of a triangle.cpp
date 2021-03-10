e6a12b9038d807e11f158eef6d3fa5d3d9bb0160
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-03-10 23:34:00 +0000

    Area of a triangle complet

---
+++ Area of a triangle.cpp
@@ -0,0 +1,12 @@
+#include < iostream.h >
+
+void main()
+{
+    int height, base;
+    float ans;/*ans may come in fractions*/
+    cout<<"Enter height and base";
+    cin>>height>>base;
+    ans= (1/2)*height*base;
+    /* mathematical formula*/
+    cout<<"Area if triangle is"<<ans;
+}
