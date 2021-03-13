1f90ccb366d899d03342f90f21d52e11fa62ba38
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-03-13 01:09:53 +0000

    added Decimal to Octal

---
+++ Decimal to Octal.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+
+void main()
+{
+    long num, decimal_num, remainder, base = 1, octal = 0;
+    cout<<"Enter a decimal integer \n";
+    cin>>amp num;
+    decimal_num = num;
+    while (num > 0)
+        {
+            remainder = num % 8;
+            octal = octal + remainder * base;
+            num = num / 8;
+            base = base * 10;
+        }
+    cout<<"Input number is ="<<decimal_num<<"\n";
+    cout<<"Its octal equivalent is ="<<Octal<<"\n";
+}
