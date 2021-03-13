8fc747b304fb39db297e18ce594c97087ba8890c
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-03-13 01:09:28 +0000

    added Decimal to Binary

---
+++ Decimal to Binary.cpp
@@ -0,0 +1,19 @@
+#include < iostream.h >
+
+int main()
+{
+    int n, c, k;
+    cout<<"Enter an integer in decimal number system\n";
+    cin>>n;
+    cout<<n<<"in binary number system is:\n";
+    for (c = 31; c >= 0; c--)
+        {
+            k = n >> c;
+            /*Right shift(Binary Divide by 2)*/
+            if (k & 1)//k is logically ANDed with 1
+                cout<<"1";
+            else
+                cout<<"0";
+        }
+    return 0;
+}
