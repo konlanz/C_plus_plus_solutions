fb93164d953f0bac5b003165fdd8445b1c19ccaf
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-03-13 01:08:41 +0000

    added binarry to Octal

---
+++ Binary to Octal.cpp
@@ -0,0 +1,17 @@
+#include < iostream.h>
+
+int main()
+{
+    long int binarynum, octalnum = 0, j = 1, remainder;
+    cout<<"Enter the value for binary number: ";
+    cin>>binarynum;
+    while (binarynum != 0)
+        {
+            remainder = binarynum % 10;
+            octalnum = octalnum + remainder * j;
+            j = j * 2;
+            binarynum = binarynum / 10;
+        }
+    cout<<"Equivalent octal value:"<<octalnum;
+    return 0;
+}
