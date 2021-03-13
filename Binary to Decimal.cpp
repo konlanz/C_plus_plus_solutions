cc27a2ef315b6ba77bd46d71308ae6322ef78c70
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-03-13 01:08:13 +0000

    added binarry to decimal

---
+++ Binary to Decimal.cpp
@@ -0,0 +1,19 @@
+#include < iostream.h >
+
+void main()
+{
+    int num, binary_val, decimal_val = 0, base = 1, rem;
+    cout<<"Enter a binary number(1s and 0s) \n";
+    cin>>amp num;
+    binary_val = num;
+    while (num > 0)
+        {
+            rem = num % 10;
+            decimal_val = decimal_val + rem * base;
+            num = num / 10 ;
+            num = num / 10 ;
+            base = base * 2;
+        }
+    cout<<"The Binary number is ="<<binary_val<<" \n";
+    cout<<"Its decimal equivalent is ="<<decimal_val<<" \n";
+}
